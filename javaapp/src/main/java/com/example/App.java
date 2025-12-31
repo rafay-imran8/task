6fa93ed01aa8bdeaf8e471bba8636fa44f148a29
Muhammad Rafay Imran <imranrafay01@gmail.com>
2025-12-31 16:17:58 +0500

    Update App.java

--- javaapp/src/main/java/com/example/App.java
+++ javaapp/src/main/java/com/example/App.java
@@ -1,5 +1,5 @@
 package com.example.helloworld;
-//hello
+//hello world
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
